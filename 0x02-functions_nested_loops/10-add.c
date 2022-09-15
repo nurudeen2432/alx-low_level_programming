9dc6cd145e74cd2684b34f2f25b4ebfac38adbd9
nurudeen2432 <nurudeendurowade@gmail.com>
2022-09-14 21:07:53 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ * main - function that adds two integers and return their result
+ * Return : 0
+*/
+
+int add (int a, int b){
+
+
+int result;
+
+result = a + b;
+
+printf(" %d \n", result);
+
+
+return (result);
+
+
+}
