722757d5fc98f78799e05f6f292e1efc8a953ea4
nurudeen2432 <nurudeendurowade@gmail.com>
2022-09-14 18:06:43 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - program that prints Holberton.
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_hbt[8] = "_putchar";
+
+	while (n < 8)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
