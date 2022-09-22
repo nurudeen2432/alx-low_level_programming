8219ec6d113ce2d97c26f7034b3367b659ef0db8
nurudeen2432 <nurudeendurowade@gmail.com>
2022-09-22 07:15:44 -0700

    To upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,32 +1,21 @@
 #include "main.h"
 
 /**
- * reverse_array - function that reverses the content of an array of integers.
- *
- * @a: pointer of array int
- * @n: the number of elements to swap
- *
- * Return: nothing
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercase.
+ * @ch: Pointer to Char
+ * Return: char.
  */
 
-void reverse_array(int *a, int n)
+char *string_toupper(char *ch)
 {
 	int i = 0;
-	int j = 0;
-	int *init, *final;
-	int aux = 0;
 
-	init = a;
-	final = a;
-	n -= 1;
-	while (i < n)
-		i += 1;
-	while (j <= i)
+	while (*(ch + i) != '\0')
 	{
-		aux = *(init + j);
-		*(init + j) = *(final + i);
-		*(final + i) = aux;
-		j++;
-		i--;
+		if (*(ch + i) >= 97 && *(ch + i) <= 122)
+			*(ch + i) = *(ch + i) - ' ';
+		i++;
 	}
+	return (ch);
 }
